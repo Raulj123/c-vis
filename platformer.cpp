61643da2f4aa42160e8703f809f95edd4e82b26d
raul <jarquinr121@gmail.com>
2023-04-06 22:34:20 -0700

    added loop counter and swap line color

--- platformer.cpp
+++ platformer.cpp
@@ -7,9 +7,10 @@
 #include <algorithm>
 #include <SDL.h>
 using namespace std;
-
+void drawState(vector<int> v, SDL_Renderer* renderer, int loopCounter, int swap);
 int main() {
-
+	
+	
 	random_device rando;				   // creating random device object
 	uniform_int_distribution<int> d(1, 99); // generates ints uniformally between 1-99
 	vector<int> v;
@@ -21,7 +22,7 @@ int main() {
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
 	SDL_CreateWindowAndRenderer(100*10, 100*10,0, &window, &renderer);		//create a window with a size of 100x100 pixels multiplied by a scaling factor of 10.
-	SDL_RenderSetScale(renderer,10,10);		//ets the scale of the renderer to 10x10, which means that anything drawn using this renderer will be scaled up by a factor of 10 in both the x and y directions.
+	SDL_RenderSetScale(renderer,10,10);		//sets the scale of the renderer to 10x10, which means that anything drawn using this renderer will be scaled up by a factor of 10 in both the x and y directions.
 	
 	//sorting..
 	for(int i =0; i <v.size() -1; i++){		// minus one because no need to loop last element (loop counter)
@@ -29,28 +30,37 @@ int main() {
 			if(v[j] > v[j+1]){				
 				swap(v[j], v[j+1]);
 			}
+			//clear Screen
+			SDL_SetRenderDrawColor(renderer,0,0,0,255);		//ombination of these two functions will set the entire renderer to black.
+			SDL_RenderClear(renderer);
+			drawState(v, renderer, i,j);	//calling function to draw lines
+			SDL_RenderPresent(renderer);
+			SDL_Delay(1);
 		}
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	// SDL_Init(SDL_INIT_EVERYTHING);
-	// SDL_Window *window= SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,800,600, SDL_WINDOW_SHOWN);
-	// SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,0);
-	// SDL_SetRenderDrawColor(renderer, 0 ,244,0,244);
-	// SDL_RenderClear(renderer);
-	// SDL_RenderPresent(renderer);
-	// SDL_Delay(3000);
 
  	return 0;
+}
+
+void drawState(vector<int> v, SDL_Renderer* renderer, int loopCounter, int swap){
+	int index = 0;
+	for(int i : v){
+		if(index == loopCounter){
+			SDL_SetRenderDrawColor(renderer, 255,165,0,255);
+		} else if(index == swap) {
+			SDL_SetRenderDrawColor(renderer, 255,192,202,255);
+		}else{
+			SDL_SetRenderDrawColor(renderer, 255,255,255,255);		//function sets the render draw color to white
+		}
+		SDL_RenderDrawLine(renderer, index, 99, index, i);
+		/*Parameters for DrawLine function: makes vertical lines depending on i value 
+		renderer =  object that you want to draw the line on
+		x = The x-coordinate of the starting point of the line.
+		y = The y-coordinate of the starting point of the line.
+		x= The x-coordinate of the ending point of the line
+		y= 	The y-coordinate of the ending point of the line.				
+		*/
+		index++;
+	}
 }
